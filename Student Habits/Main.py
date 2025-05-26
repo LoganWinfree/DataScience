d4ce52ca43f8fac684c05f87ec81c75631655bba
Logan <loganwinfree4@gmail.com>
2025-05-26 13:39:40 -0400

    convert to dummy variables

--- Student Habits/Main.py
+++ Student Habits/Main.py
@@ -3,10 +3,17 @@ import pandas as pd
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 
-data = pd.read_csv("DataScience/Student Habits/student_habits_performance.csv")
 
-x = data.drop('exam_score', axis=1)
+reg = linear_model.LinearRegression()
+
+data = pd.read_csv("DataScience/Student Habits/student_habits_performance.csv")
+data = pd.get_dummies(data, columns=["gender", "part_time_job", "diet_quality", "parental_education_level", "internet_quality", "extracurricular_participation"], drop_first=True)
+x = data.drop('exam_score', axis=1).drop('student_id', axis=1)
 y = data['exam_score']
 
 train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.1)
+
+reg.fit(train_x, train_y)
+
+predict_y = reg.predict(test_x)
 
