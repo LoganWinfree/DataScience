ff4b232d3a4f04c067b595edbf2763c9e896e626
Logan <loganwinfree4@gmail.com>
2025-05-27 16:14:01 -0400

    Predict (Not best)

--- Heart Attack/HeartAttack.py
+++ Heart Attack/HeartAttack.py
@@ -5,5 +5,22 @@ from sklearn.model_selection import train_test_split
 import sklearn.metrics as skm
 import math
 
+reg = linear_model.LogisticRegression(solver="newton-cholesky")
+
 data = pd.read_csv("DataScience/Heart Attack/Medicaldataset.csv")
-print(data.head())
+
+data = pd.get_dummies(data, columns=["Result"], drop_first=True) #Result_positive
+
+x = data.drop("Result_positive", axis=1)
+y = data["Result_positive"]
+
+train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.1)
+
+reg.fit(train_x, train_y)
+
+predict_y = reg.predict(test_x)
+
+print("R Square: ") 
+print(round(skm.r2_score(test_y, predict_y), 3))
+print("\nRoot MSE: ")
+print(round(skm.root_mean_squared_error(test_y, predict_y), 3))
